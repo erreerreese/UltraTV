20a02747cdb5c35535c5bd3b308168edd7caa376
erreerreese <a01191478@itesm.mx>
2014-10-19 02:27:37 -0500

    cards

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@
  */
 
 var UI = require('ui');
-
+var remote;
+var quotes;
 var menu = new UI.Menu({
   sections: [{
     title: 'UltraTV',
@@ -18,5 +19,25 @@ var menu = new UI.Menu({
          icon: 'images/javs.png'
         }]
   }]
+});
+menu.on('select',function(e){
+   switch(e.itemIndex){
+  case 0:
+    remote=new UI.Card({
+       title:'Remote',
+       subtitle:'nombre del programa',
+       body: 'nombre del canal'
+    });
+    remote.show();
+    break;
+  case 1:
+    quotes=new UI.Card({
+       title:'Quotes',
+       subtitle:'insert random quote',
+       body: 'categoria del programa'
+    });
+    quotes.show();
+    break;
+  }
 });
 menu.show();
